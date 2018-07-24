cd7d2ccc2c03dc9c804e5444170fee786ba76841
Preety Kumari <preetydakshana@gmail.com>
2018-07-24 15:36:09 +0530

    updated solution

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -5,19 +5,11 @@ namespace HoloalphabeticSentence
 {
     public class HoloalphabeticSentence
     {
-        // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
-        public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
-        {
+		// Write the logic to determine whether the input string is a HoloalphabeticSentence or not
+		public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
+		{
 			string str = input.ToLower();
-			Boolean flag = true;
-			foreach (char c  in alphabets) {
-				if ((str.IndexOf(c) == -1)) {
-					flag = false;
-					break;
-				}
-			}
-
-            return flag;
-        }
+			return alphabets.All(c => str.Contains(c));
+		}
     }
 }
